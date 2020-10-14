daa7a8d0b60b0d3d3e977d3da2e020c1016b5c1a
lautaro <lgalende@itba.edu.ar>
2020-10-14 12:50:22 -0300

    Modificaciones en exp_of_block_needed y split_node. Malloc no funciona

--- buddy2.c
+++ buddy2.c
@@ -11,8 +11,9 @@
 #define MIN_BLOCK_SIZE      4096 //2^12 = 4K
 #define MIN_BLOCK_SIZE_EXP  12
 
-#define MEM_FIRST_ADDRESS  0x600000 //esta bien la dir?
+#define MEM_FIRST_ADDRESS  0x600000 //EN NINGUN MOMENTO NOS ASEGURAMOS DE QUE LA MEM EN ESTAS POSICIONES NO ESTE OCUPADA
 
+/*
 #define NODES_QTY() {                                           \
   int suma = 0;                                                 \
   for( i = 0; i < MEM_SIZE_EXP - MIN_BLOCK_SIZE_EXP + 1; i++){  \
@@ -20,7 +21,7 @@
   }                                                             \
   return suma;                                                  \
 } //no funca
-
+*/
 
 
 typedef struct node{ 
@@ -28,14 +29,15 @@ typedef struct node{
   struct node *left; 
   struct node *right;
   struct node *parent;  //tal vez no lo usemos
-  void *ptr;
-  int exp; //size = 2^exp
-  int idx;
+  void *ptr;            //memory direction to be returned in malloc
+  int exp;              //size = 2^exp
+  int idx;              //indice donde esta guardada la estructura - mem[idx]
+  int array_ptr_idx     //indice de la lista en la que esta el nodo - block_arrays_ptrs[array_ptr_idx]
 }node;
 
 
-static node mem[]; //Initialized in main function.
-static node root = {FREE, NULL, NULL, NULL, MEM_FIRST_ADDRESS, MEM_SIZE_EXP, 0}; //O LO DECLARAMOS COMO *node
+node mem[24600]; //Initialized in main function (NOT ANY MORE). 24600 = nodes_qty * sizeof(node) 
+static node root = {FREE, mem+1, mem+2, NULL, (void *)MEM_FIRST_ADDRESS, MEM_SIZE_EXP, 0, 0}; //O LO DECLARAMOS COMO *node
 void *block_arrays_ptrs[MEM_SIZE_EXP - MIN_BLOCK_SIZE_EXP + 1]; //Initialized in main function.
 int block_arrays_sizes[MEM_SIZE_EXP - MIN_BLOCK_SIZE_EXP + 1] = {0};
 
@@ -51,11 +53,11 @@ unsigned long nodes_qty(){
 
 
 unsigned int exp_of_block_needed(unsigned int request){
-  unsigned int exp = MEM_SIZE_EXP; //-1 ?
+  unsigned int exp = MIN_BLOCK_SIZE_EXP;
   unsigned int size = MIN_BLOCK_SIZE;
 
   while (size < request){
-    exp--;
+    exp++;
     size *= 2;
   }
 
@@ -63,29 +65,64 @@ unsigned int exp_of_block_needed(unsigned int request){
 }
 
 
+unsigned int exp_of_block_needed2(unsigned int request) {
+    int i;
+    for (i = 0; 1 << i < request; i++)
+        ;
+
+    return i >= MIN_BLOCK_SIZE_EXP ? i : MIN_BLOCK_SIZE_EXP;
+}
+
+
 void split_node(node *n){ //REVISAR ESTA FUNCION (PUNTEROS, DIRECCIONES, ETC) !!!
   //como inicializamos los hijos???
-  int i = n->idx;
-  *n->left = mem[2*i + 1];
-  *n->right = mem[2*i + 2];
+  int i = n->idx, idx_aux = 2*i+1, child_arr_ptr_idx = n->array_ptr_idx + 1;
+            printf("ok%d\n", i);
+
+  mem[2*i + 1] = *n->left;
+  mem[2*i + 2] = *n->right;
   
+          printf("okkk%d\n", i);
+
+
   (*n->left).state = FREE;
-  (*n->left).left = NULL;
-  (*n->left).right = NULL;
+  (*n->left).left = mem + (2*idx_aux+1);
+  (*n->left).right = mem + (2*idx_aux+2);
   (*n->left).parent = n;
   (*n->left).ptr = n->ptr;  //chequear direccion de inicio
   (*n->left).exp = n->exp-1;
-  (*n->left).idx = 2*i+1;
+  (*n->left).idx = idx_aux;
+  (*n->left).array_ptr_idx = child_arr_ptr_idx;
   
+  idx_aux = 2*i+2;
+
+            printf("ok2-%d\n", i);
+
+
   (*n->right).state = FREE;
-  (*n->right).left = NULL;
-  (*n->right).right = NULL;
+  (*n->right).left = mem + (2*idx_aux+1);
+  (*n->right).right = mem + (2*idx_aux+2);
   (*n->right).parent = n;
   (*n->right).ptr = n->ptr + (1 << (n->exp-1));  //left node begin address + size of (left) block
   //chequear direccion de inicio
   (*n->right).exp = n->exp-1;
-  (*n->right).idx = 2*i+2;
+  (*n->right).idx = idx_aux;
+  (*n->right).array_ptr_idx = child_arr_ptr_idx;
+
+              printf("ok3-%d\n", i);
+
+  i = get_idx_of_next_available_node(child_arr_ptr_idx);
+  //block_arrays_ptrs[child_arr_ptr_idx] + i = (void*) n->left;
+  void *a = block_arrays_ptrs[child_arr_ptr_idx];
+  a+=i;
+  a = (void*)n->left;
   
+  i = get_idx_of_next_available_node(child_arr_ptr_idx);
+  //block_arrays_ptrs[child_arr_ptr_idx] + i = (void*) n->right;
+  a = block_arrays_ptrs[child_arr_ptr_idx];
+  a+=i;
+  a = (void*)n->right;
+
   n->state = SPLIT;
 }
 
@@ -118,16 +155,30 @@ void *buddy_malloc(unsigned int size_request){
   if(block_arrays_sizes[idx] == 0)
     return NULL;  //idx=0 ==> todas las listas estaban vacias
   
+        printf("ok\n");
+
   while(idx < MEM_SIZE_EXP - exp_request){
     unsigned int i = get_idx_of_next_available_node(idx);
+            printf("%d\n", i);
+
     split_node( (node*) (block_arrays_ptrs[idx] + i) );
+            printf("ok split\n");
+
     block_arrays_sizes[idx]--;
     idx++;
     block_arrays_sizes[idx]+=2;
   }
 
+        printf("ok\n");
+
+
   unsigned int i = get_idx_of_next_available_node(idx);
+  printf("fin1\n");
+
   ( (node*) (block_arrays_ptrs[idx] + i) )->state = OCCUPIED;
+
+        printf("fin2\n");
+
 
   return ( (node*) (block_arrays_ptrs[idx] + i) )->ptr;
 
@@ -190,7 +241,8 @@ int main(){
   /* Memory for nodes initialization */
   long size = sizeof(node) * nodes_qty();
   node aux[size];
-  *mem = *aux;
+          printf("%d %d %d\n", size, sizeof(node), nodes_qty());
+  //*mem = *aux;
 
   /* Set first node */
   mem[0] = root;
@@ -201,6 +253,14 @@ int main(){
     block_arrays_ptrs[level] = mem + (j * sizeof(node));
   }
   block_arrays_sizes[0] = 1;
+
+  /* TESTS */
+  for(int size = MEM_SIZE+1000; size >= 512; size/=2)
+    printf("%d (%d) (%d)\n", size, exp_of_block_needed(size), exp_of_block_needed2(size));
+
+  void *a = buddy_malloc(4096);
+
+  //buddy_free(a);
 
   return 0;
 }
